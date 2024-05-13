cd838ee4a960976f773a528f2fe2f4236f4c504b
asif883 <asifiqbal95863@gmail.com>
2024-05-13 23:46:18 +0600

    add env

--- public/firebase.config.js
+++ public/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAFYUp_OTBqfDzi3QvMWCUXTmiQ3DIMoE",
-  authDomain: "career-compass-d0501.firebaseapp.com",
-  projectId: "career-compass-d0501",
-  storageBucket: "career-compass-d0501.appspot.com",
-  messagingSenderId: "1007598019611",
-  appId: "1:1007598019611:web:05a3324c97db3879fd44be"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
